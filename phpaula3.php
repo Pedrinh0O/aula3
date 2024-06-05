3ad94c49c52c9747b49255566c96e69b18ce4832
Pedrinh0O <165160365+Pedrinh0O@users.noreply.github.com>
2024-06-05 09:45:53 -0300

    Add files via upload

---
+++ phpaula3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php
+
+        $listaAlunoIdade - array(
+            "leonardo" => 31,
+            "gabriela" => 27,
+            "bianca" => 07
+        );
+        //array apenas com valor
+        $listaCores = array(
+            "vermelho",
+            "azul",
+            "laranja"
+        );
+
+echo     $listaAlunoIdade ["leonardo"];
+echo "<br/>";
+
+echo $listaCores [2];
+
+echo "br;>";
+
+var_dump($listaAlunoIdade);
+
+echo "<br />";
+
+var_dump($listaCores);
+
+
+    ?>
+    
+</body>
+</html>
